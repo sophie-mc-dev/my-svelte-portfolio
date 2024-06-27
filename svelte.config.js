33f367be7dbaef42f18d9681081fce1192a03353
Sofia Merino Costa <scosta.geprix@gmail.com>
2024-06-27 18:18:02 +0100

    github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-node";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
+const dev = process.argv.includes("dev");
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
@@ -8,6 +10,9 @@ const config = {
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter(),
+    paths: {
+      base: dev ? "" : process.env.BASE_PATH,
+    },
   },
   preprocess: vitePreprocess(),
 };
