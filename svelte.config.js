0a79a5014775c005df8f9681b4285854279b208c
Sofia Merino Costa <scosta.geprix@gmail.com>
2025-04-17 16:33:14 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,18 @@
-import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes("dev");
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null // use 'index.html' if using SPA-style routing
+    }),
     paths: {
-      base: dev ? "" : process.env.BASE_PATH,
+      base: dev ? '' : process.env.BASE_PATH || '',
     },
   },
   preprocess: vitePreprocess(),
