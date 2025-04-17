9fabc7af1cf6662ace61d6e8f43d108bef89d0f1
Sofia Merino Costa <scosta.geprix@gmail.com>
2025-04-17 16:51:29 +0100

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.argv.includes('dev');
@@ -6,14 +6,7 @@ const dev = process.argv.includes('dev');
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: null // use 'index.html' if using SPA-style routing
-    }),
-    paths: {
-      base: dev ? '' : process.env.BASE_PATH || '',
-    },
+    adapter: adapter(),
   },
   preprocess: vitePreprocess(),
 };
