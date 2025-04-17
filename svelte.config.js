258dcb35f89ae91f19b96f9f74d6a784e5b34489
Sofia Merino Costa <scosta.geprix@gmail.com>
2025-04-17 22:14:35 +0100

    idek

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
 import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     adapter: adapter(),
